70074830133565accdfd59bea9ac6b193c6528b9
Ada <82273499+swimmingAda@users.noreply.github.com>
2021-11-08 20:53:53 +0800

    Create UserListServlet.java

---
+++ java/servlet/UserListServlet.java
@@ -0,0 +1,15 @@
+@WebServlet(name = "userListServlet", value = "/userListServlet")
+public class UserListServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        UserService userService = new UserServiceImpl();
+        List<User> users = userService.findAll();
+        request.setAttribute("user", users);
+        request.getRequestDispatcher("/list.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+}
